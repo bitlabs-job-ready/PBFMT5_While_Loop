bbcf4e091b5033a262e4ea237bf7d01935acad27
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-03-25 19:01:10 +0530

    Add files via upload

---
+++ PBFMT5_While_Loop.py
@@ -0,0 +1,13 @@
+# While Loop
+"""
+Write a program, which asks for the initial balance K0 and for the interest rate. The program shall calculate the new capital K1 after one year including the interest.
+Extend the program with a while-loop, so that the capital Kn after a period of n years can be calculated.
+
+"""
+
+#inputs
+
+K = float(input("Starting capital? "))
+p = float(input("Interest rate? "))
+n = int(input("Number of years? "))
+
